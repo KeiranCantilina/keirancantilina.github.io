334d646493bd268fd551c73cd9ec8c6e6686d69f
KeiranCantilina <keirancantilina@gmail.com>
2023-10-26 00:15:26 -0400

    Add files via upload

--- project/WebOBJTest/code/js/objviewer.js
+++ project/WebOBJTest/code/js/objviewer.js
@@ -1,6 +1,9 @@
 // Written by Keiran Cantilina 
 // 2023-10-25
 
+import OBJLoader from '.\objloader.js'
+import MTLLoader from '.\mtlloader.js'
+
 // Interface with html
 function OBJViewerEnable(classname){
 	var models=document.getElementsByClassName(classname);
@@ -9,13 +12,16 @@ function OBJViewerEnable(classname){
 	}
 }
 
+var WEBGL={isWebGLAvailable:function(){try{var canvas=document.createElement('canvas');return!!(window.WebGLRenderingContext&&(canvas.getContext('webgl')||canvas.getContext('experimental-webgl')));}catch(e){return false;}},isWebGL2Available:function(){try{var canvas=document.createElement('canvas');return!!(window.WebGL2RenderingContext&&canvas.getContext('webgl2'));}catch(e){return false;}},getWebGLErrorMessage:function(){return this.getErrorMessage(1);},getWebGL2ErrorMessage:function(){return this.getErrorMessage(2);},getErrorMessage:function(version){var names={1:'WebGL',2:'WebGL 2'};var contexts={1:window.WebGLRenderingContext,2:window.WebGL2RenderingContext};var message='Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>';var element=document.createElement('div');element.id='webglmessage';element.style.fontFamily='monospace';element.style.fontSize='13px';element.style.fontWeight='normal';element.style.textAlign='center';element.style.background='#fff';element.style.color='#000';element.style.padding='1.5em';element.style.width='400px';element.style.margin='5em auto 0';if(contexts[version]){message=message.replace('$0','graphics card');}else{message=message.replace('$0','browser');}
+message=message.replace('$1',names[version]);element.innerHTML=message;return element;}};
+
 function OBJViewer(elem,model,mtlsrc){
 	
 	// Throw error message if no webgl
-	//if(!WEBGL.isWebGLAvailable()){
-	//	elem.appendChild(WEBGL.getWebGLErrorMessage());
-	//	return;
-	//}
+	if(!WEBGL.isWebGLAvailable()){
+		elem.appendChild(WEBGL.getWebGLErrorMessage());
+		return;
+	}
 	
 	// Make renderer, camera, and scene objects and configure them
 	var renderer=new THREE.WebGLRenderer({antialias:true,alpha:true});
@@ -37,12 +43,12 @@ function OBJViewer(elem,model,mtlsrc){
 	scene.add(new THREE.HemisphereLight(0xffffff,0x080820,1.5));
 	
 	// Load MTL file
-	new THREE.MTLLoader().load(mtlsrc, function ( materials ) {
+	new MTLLoader().load(mtlsrc, function ( materials ) {
 
 		materials.preload();
 		
 		// Load OBJ File (using loaded MTL file materials)
-		new THREE.OBJLoader()
+		new OBJLoader()
 			.setMaterials( materials )
 			.load(model, function ( object ) {
 				
