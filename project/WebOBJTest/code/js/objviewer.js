e8b767136bf643b5f3a559175627547fcbd19811
KeiranCantilina <keirancantilina@gmail.com>
2023-10-26 01:15:54 -0400

    Add files via upload

--- project/WebOBJTest/code/js/objviewer.js
+++ project/WebOBJTest/code/js/objviewer.js
@@ -3,6 +3,7 @@
 
 import { MTLLoader } from './mtlloader.js'
 
+// Globals
 var camera=new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20 );
 var scene=new THREE.Scene();
 var renderer = new THREE.WebGLRenderer( { antialias: true, alpha:true} );
@@ -16,11 +17,12 @@ function OBJViewerEnable(classname){
 	}
 }
 
+// WebGL check cribbed from minimized stlviewer implementation
 var WEBGL={isWebGLAvailable:function(){try{var canvas=document.createElement('canvas');return!!(window.WebGLRenderingContext&&(canvas.getContext('webgl')||canvas.getContext('experimental-webgl')));}catch(e){return false;}},isWebGL2Available:function(){try{var canvas=document.createElement('canvas');return!!(window.WebGL2RenderingContext&&canvas.getContext('webgl2'));}catch(e){return false;}},getWebGLErrorMessage:function(){return this.getErrorMessage(1);},getWebGL2ErrorMessage:function(){return this.getErrorMessage(2);},getErrorMessage:function(version){var names={1:'WebGL',2:'WebGL 2'};var contexts={1:window.WebGLRenderingContext,2:window.WebGL2RenderingContext};var message='Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>';var element=document.createElement('div');element.id='webglmessage';element.style.fontFamily='monospace';element.style.fontSize='13px';element.style.fontWeight='normal';element.style.textAlign='center';element.style.background='#fff';element.style.color='#000';element.style.padding='1.5em';element.style.width='400px';element.style.margin='5em auto 0';if(contexts[version]){message=message.replace('$0','graphics card');}else{message=message.replace('$0','browser');}
 message=message.replace('$1',names[version]);element.innerHTML=message;return element;}};
 
 
-
+// Main
 function OBJViewer(elem,model,mtlsrc){
 	
 	// Throw error message if no webgl
@@ -37,6 +39,18 @@ function OBJViewer(elem,model,mtlsrc){
 	
 	camera.add(new THREE.HemisphereLight(0xffffff,0x080820,1.5));
 	scene.add( camera );
+	
+	// Log loading progress
+	const onProgress = function ( xhr ) {
+
+					if ( xhr.lengthComputable ) {
+
+						const percentComplete = xhr.loaded / xhr.total * 100;
+						console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+
+					}
+
+				};
 	
 	// Load MTL file
 	new MTLLoader().load(mtlsrc, function ( materials ) {
@@ -90,16 +104,12 @@ function onWindowResize() {
 	camera.updateProjectionMatrix();
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
-			//
-
+// Can't remember why I subset this
 function animate() {
-
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-
 }
 
 // Make sure we run when the window loads
